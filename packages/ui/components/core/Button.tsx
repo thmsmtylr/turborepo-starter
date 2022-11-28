52256ee33c65c1c7db6499860e60d45a0105b0ad
Thomas Taylor <thmsmtylr@gmail.com>
2022-11-28 19:27:42 +1100

    refactor: button example

--- packages/ui/components/core/Button.tsx
+++ packages/ui/components/core/Button.tsx
@@ -1,35 +1,10 @@
 import { forwardRef, MouseEvent } from "react";
 import { classNames } from "../../helpers/classNames";
 
-export enum ButtonSize {
-  SM = "SM",
-  MD = "MD",
-  LG = "LG",
-  XL = "XL",
-  XXL = "XXL",
-}
-
-export enum ButtonHierarchy {
-  Primary = "Primary",
-  SecondaryColor = "SecondaryColor",
-  SecondaryGrey = "SecondaryGrey",
-  TertiaryColor = "TertiaryColor",
-  TertiaryGrey = "TertiaryGrey",
-  LinkColor = "LinkColor",
-  LinkGrey = "LinkGrey",
-}
-
-export enum ButtonState {
-  Default = "Default",
-  Hover = "Hover",
-  Focused = "Focused",
-  Disabled = "Disabled",
-}
+export type ButtonSize = "sm" | "md" | "lg" | "xl" | "xxl";
 
 export interface ButtonProps {
   size: ButtonSize;
-  hierarchy: ButtonHierarchy;
-  state: ButtonState;
   label: string;
   onClick: (e: MouseEvent<HTMLButtonElement>) => unknown;
   disabled?: boolean;
@@ -45,9 +20,8 @@ export type ButtonRef = HTMLButtonElement;
  */
 export const Button = forwardRef<ButtonRef, ButtonProps>((props, ref) => {
   const {
-    size = ButtonSize.SM,
-    hierarchy = ButtonHierarchy.Primary,
-    state = ButtonState.Default,
+    size = "sm",
+    disabled = false,
     label,
     onClick,
     type = "button",
@@ -58,13 +32,13 @@ export const Button = forwardRef<ButtonRef, ButtonProps>((props, ref) => {
       ref={ref}
       type={type}
       onClick={(e) => onClick(e)}
-      disabled={state === ButtonState.Disabled}
+      disabled={disabled}
       className={classNames(
-        size === ButtonSize.SM ? "px-3.5 py-2" : "",
-        size === ButtonSize.MD ? "px-4 py-2.5" : "",
-        size === ButtonSize.LG ? "px-4.5 py-2.5" : "",
-        size === ButtonSize.XL ? "px-5 py-3" : "",
-        size === ButtonSize.XXL ? "px-7 py-4" : "",
+        size === "sm" ? "px-3.5 py-2" : "",
+        size === "md" ? "px-4 py-2.5" : "",
+        size === "lg" ? "px-4.5 py-2.5" : "",
+        size === "xl" ? "px-5 py-3" : "",
+        size === "xxl" ? "px-7 py-4" : "",
         // Default
         "gap-2 bg-black rounded-lg text-white text-sm font-medium flex items-center justify-center"
       )}
