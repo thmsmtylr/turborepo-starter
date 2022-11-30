5e0442a1742ad2a91c215c8a9deec33812aa8907
Thomas Taylor <thmsmtylr@gmail.com>
2022-11-30 20:29:07 +1100

    feat: try vercel deploy again

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -2,4 +2,8 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 });
