33a232cf26c597e3cf1f8150225d5fe485509486
Thomas Taylor <thmsmtylr@gmail.com>
2022-11-30 20:31:36 +1100

    test: undo deploy test

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -2,8 +2,4 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
 });
